9f7ed3813cd713f264d1bbf3c64be9dd9d0a7e5e
Silavsale <silavsale@gmail.com>
2020-12-25 21:47:05 -0500

    completed advanced types section

--- README.md
+++ README.md
@@ -5,3 +5,24 @@ Understand what TypeScript really is about and how it works
 Why TypeScript offers a real advantage over vanilla JavaScript
 Learn TypeScript both in theory as well as applied to real use-cases and projects
 Learn how to combine TypeScript with ReactJS or NodeJS / Express
+
+
+# Course Content
+
+- Section 1: Getting Started
+
+- Section 2: TypeScript Basics & Basic Types
+
+- Section 3: The TypeScript Compiler (and its Configuration)
+
+- Section 4: Next-generation JavaScript & TypeScript
+
+- Section 5: Classes & Interfaces
+
+- Section 6: Advanced Types
+
+- Section 7: Generics
+
+- Section 8: Decorators
+
+- Section 9: Practice Time! Let's build a Drag & Drop Project
--- src/classes.ts
+++ src/classes.ts
@@ -1,112 +1,112 @@
-abstract class Department {
-    static fiscalYear = 2020
-    // name: string
-    protected employees: string[] = []
+// abstract class Department {
+//     static fiscalYear = 2020
+//     // name: string
+//     protected employees: string[] = []
     
-    constructor(protected readonly id: string, public name: string){
-        // this.name = n
-    }
+//     constructor(protected readonly id: string, public name: string){
+//         // this.name = n
+//     }
 
-    static createEmployee(name: string) {
-        return {name: name}
-    }
+//     static createEmployee(name: string) {
+//         return {name: name}
+//     }
 
-    abstract describe(this: Department): void
+//     abstract describe(this: Department): void
 
-    addEmployee(employees: string){
-        this.employees.push(employees)
-    }
+//     addEmployee(employees: string){
+//         this.employees.push(employees)
+//     }
 
-    printEmployeeInformation() {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    }
-}
+//     printEmployeeInformation() {
+//         console.log(this.employees.length);
+//         console.log(this.employees);
+//     }
+// }
 
-class ITDepartment extends Department {
-    admins: string[]
-    constructor(id: string, admins: string[]){
-        super(id, 'IT')
-        this.admins = admins
-    }
+// class ITDepartment extends Department {
+//     admins: string[]
+//     constructor(id: string, admins: string[]){
+//         super(id, 'IT')
+//         this.admins = admins
+//     }
 
-    describe() {
-        console.log('IT Department - ID: ' + this.id);
-    }
-}
+//     describe() {
+//         console.log('IT Department - ID: ' + this.id);
+//     }
+// }
 
-class AccountingDepartment extends Department {
-    private lastReport: string
-    private static instance: AccountingDepartment
+// class AccountingDepartment extends Department {
+//     private lastReport: string
+//     private static instance: AccountingDepartment
 
-    get mostRecentReport () {
-        if (this.lastReport) {
-            return this.lastReport
-        }
-        throw new Error("No report found.");
-    }
+//     get mostRecentReport () {
+//         if (this.lastReport) {
+//             return this.lastReport
+//         }
+//         throw new Error("No report found.");
+//     }
 
-    set mostRecentReport(value: string) {
-        if(!value) {
-            throw new Error("Please pass in a valid value!");
-        }
-    }
+//     set mostRecentReport(value: string) {
+//         if(!value) {
+//             throw new Error("Please pass in a valid value!");
+//         }
+//     }
 
-    private constructor(id: string, private reports: string[]){
-        super(id, 'Accounting')
-        this.lastReport = reports[0]
-    }
+//     private constructor(id: string, private reports: string[]){
+//         super(id, 'Accounting')
+//         this.lastReport = reports[0]
+//     }
 
-    static getInstance() {
-        if (AccountingDepartment.instance){
-            return this.instance
-        }
-        this.instance = new AccountingDepartment('d2', [])
-        return this.instance 
-    }
+//     static getInstance() {
+//         if (AccountingDepartment.instance){
+//             return this.instance
+//         }
+//         this.instance = new AccountingDepartment('d2', [])
+//         return this.instance 
+//     }
 
-    describe() {
-        console.log('Accounting Department - ID:' + this.id);
-    }
+//     describe() {
+//         console.log('Accounting Department - ID:' + this.id);
+//     }
 
-    addEmployee(name: string){
-        if(name === 'Max'){
-            return
-        }
-        this.employees.push(name)
-    }
+//     addEmployee(name: string){
+//         if(name === 'Max'){
+//             return
+//         }
+//         this.employees.push(name)
+//     }
 
-    addReport(text: string) {
-        this.reports.push(text)
-        this.lastReport = text
-    }
+//     addReport(text: string) {
+//         this.reports.push(text)
+//         this.lastReport = text
+//     }
 
-    printReport() {
-        console.log(this.reports);
-    }
-}
+//     printReport() {
+//         console.log(this.reports);
+//     }
+// }
 
-// const devs = new Department("Devs")
-
-
-const empoloyee1 = Department.createEmployee('Max')
-console.log('empoloyee1', empoloyee1);
+// // const devs = new Department("Devs")
 
 
-const it = new ITDepartment('d1', ['Max'])
-
-it.addEmployee('Max')
-it.addEmployee('Fridrich')
-
-it.describe()
-it.name = 'NEW NAME'
-it.printEmployeeInformation()
-
-console.log(it);
-
-const accounting = AccountingDepartment.getInstance()
-
-// const accounting = new Accounting('d2', [])
+// const empoloyee1 = Department.createEmployee('Max')
+// console.log('empoloyee1', empoloyee1);
 
 
-// console.log(devs);
+// const it = new ITDepartment('d1', ['Max'])
+
+// it.addEmployee('Max')
+// it.addEmployee('Fridrich')
+
+// it.describe()
+// it.name = 'NEW NAME'
+// it.printEmployeeInformation()
+
+// console.log(it);
+
+// const accounting = AccountingDepartment.getInstance()
+
+// // const accounting = new Accounting('d2', [])
+
+
+// // console.log(devs);
